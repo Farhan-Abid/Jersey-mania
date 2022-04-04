f5b058c8b8edfe8165797a2a201bed8953d692c0
Farhan-Abid <shahabid628@gmail.com>
2022-04-05 01:41:21 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome to Jersey Mania!</h2>
+            <nav>
+                
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Header/JerseyHome/JerseyHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JerseyHome = () => {
+    return (
+        <div>
+            <h2>Home of jersey!</h2>
+        </div>
+    );
+};
+
+export default JerseyHome;
